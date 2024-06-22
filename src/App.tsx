5dc6d832b80ff1a6cb752ddc438ec73c597d7fea
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-06-22 20:11:03 +0200

    added dark and light mode to favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import Time from "@/components/time/time"
 import Profile from "@/components/profile/profile"
 import Location from "@/components/location/location"
+import { useEffect } from "react"
 
-function App() {
+const App = () => {
+    useEffect(() => {
+        const changeFavicon = (theme: string) => {
+            const favicon = document.querySelector("link[rel='icon']") as HTMLLinkElement;
+            if (favicon) {
+                favicon.href = theme === 'dark' ? '/favicon-dark.png' : '/favicon-light.png';
+            }
+        };
+
+        const mediaMatch = window.matchMedia('(prefers-color-scheme: dark)');
+        const theme = mediaMatch.matches ? 'dark' : 'light';
+
+        changeFavicon(theme);
+        mediaMatch.addEventListener('change', () => changeFavicon(theme));
+    }, []);
+
     return (
         <>
             <Time />
