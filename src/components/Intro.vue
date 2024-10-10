165943ac51f289693549f2d946d3109cc8d58318
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-10-10 20:13:01 +0200

    initial commit for website-remake

---
+++ src/components/Intro.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+defineProps<{ forename: string, surename: string }>()
+</script>
+
+<template>
+  <h1>
+    I'm {{ forename }}, computer science student based in <span>Austria</span>
+  </h1>
+</template>
+
+<style scoped>
+h1,
+span {
+  font-size: 3.5rem;
+  font-weight: 525;
+  line-height: 1.2;
+  letter-spacing: -0.02rem;
+  padding: 0;
+  margin: 0;
+}
+
+span {
+  background: linear-gradient(74deg, #4285f4 0, #9b72cb 9%, #d96570 20%, #d96570 24%, #9b72cb 35%, #4285f4 44%, #9b72cb 50%, #d96570 56%, #131314 75%, #131314 100%);
+  background-size: 400% 100%;
+  -webkit-background-clip: text;
+  background-clip: text;
+  color: transparent;
+}
+
+@media screen and (max-width: 1400px) {
+  h1, span {
+    font-size: 3rem;
+    font-weight: 550;
+  }
+}
+
+@media screen and (max-width: 1000px) {
+  h1,
+  span {
+    font-size: 2.75rem;
+  }
+}
+
+@media screen and (max-width: 650px) {
+  h1,
+  span {
+    font-size: 2.5rem;
+  }
+}
+
+@media screen and (max-width: 500px) {
+  h1,
+  span {
+    font-size: 2.25rem;
+  }
+}
+</style>
---
+++ src/components/Pill.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+defineProps<{ text: string }>()
+</script>
+
+<template>
+  <div class="pill">
+    <div class="dot"></div>
+    <span>{{ text }}</span>
+  </div>
+</template>
+
+<style scoped>
+.pill {
+  display: inline-flex;
+  align-items: center;
+  padding: 0.5rem 1rem;
+  border-radius: 20px;
+  font-weight: 350;
+  user-select: none;
+  background-color: #f2fff0;
+  color: #00a605;
+}
+
+
+.dot {
+  display: inline-block;
+  width: 0.55rem;
+  height: 0.55rem;
+  margin-right: 0.5rem;
+  border-radius: 100%;
+  background-color: #048608;
+}
+</style>
