6a1f7ad32f433f2131f5902aa5c5c4776e62dd30
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-10-13 15:30:07 +0200

    refactored AvailabilityPill component
    + dynamic dot animation
    + updated font-weight and font-color

--- src/components/AvailabilityPill.vue
+++ src/components/AvailabilityPill.vue
@@ -1,14 +1,33 @@
 <script setup lang="ts">
-defineProps<{
+import { ref, onMounted } from 'vue';
+
+const props = defineProps<{
   available: boolean,
   availableText: string,
   unavailableText: string,
 }>();
+
+const animateDot = ref(false);
+
+const triggerAnimation = () => {
+  animateDot.value = true;
+  setTimeout(() => {
+    animateDot.value = false;
+  }, 400);
+};
+
+onMounted(() => {
+  if (props.available) {
+    setInterval(triggerAnimation, 2000);
+  }
+});
 </script>
 
 <template>
   <div class="pill" :class="available ? 'available' : 'unavailable'">
-    <div class="dot" :class="available ? 'available' : 'unavailable'"></div>
+    <div class="dot" :class="available ? 'available' : 'unavailable'">
+      <div v-if="animateDot" class="animate"></div>
+    </div>
     <span>{{ available ? availableText : unavailableText }}</span>
   </div>
 </template>
@@ -19,13 +38,14 @@ defineProps<{
   align-items: center;
   padding: 0.5rem 1rem;
   border-radius: 20px;
-  font-weight: 350;
+  font-weight: 425;
   user-select: none;
+  position: relative;
 }
 
 .pill.available {
   background-color: #f2fff0;
-  color: #00a605;
+  color: #009b05;
 }
 
 .pill.unavailable {
@@ -37,8 +57,9 @@ defineProps<{
   display: inline-block;
   width: 0.5rem;
   height: 0.5rem;
-  margin-right: 0.5rem;
+  margin-right: 0.55rem;
   border-radius: 100%;
+  position: relative;
 }
 
 .dot.available {
@@ -47,5 +68,25 @@ defineProps<{
 
 .dot.unavailable {
   background-color: #a60005;
+}
+
+@keyframes expand {
+  0% {
+    transform: scale(1);
+    opacity: 1;
+  }
+  100% {
+    transform: scale(3.2);
+    opacity: 0;
+  }
+}
+
+.animate {
+  animation: expand 400ms ease-out;
+  position: absolute;
+  width: 0.5rem;
+  height: 0.5rem;
+  border-radius: 50%;
+  background: rgba(0, 0, 0, 0.4);
 }
 </style>
