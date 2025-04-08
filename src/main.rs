d1f02a4adf0e051f03c17b5013e966fd6efada63
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-04-08 18:41:32 +0200

    feat: add basic yew app

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use yew::prelude::*;
+
+#[function_component]
+fn App() -> Html {
+    html! {
+        <div>
+            <h1>{ "Hello Yew" }</h1>
+        </div>
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
