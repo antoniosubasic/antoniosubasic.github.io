6ebda844fe9d45b01f3919b553f3caeda55f6a65
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-04-10 17:53:01 +0200

    style: organize css-classes by width, text and other definitions to improve readability

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,36 @@ use components::availability_pill::AvailabilityPill;
 #[function_component]
 fn App() -> Html {
     html! {
-        <main class="max-h-screen max-w-[23.15rem] sm:max-w-xl md:max-w-3xl xl:max-w-7xl flex flex-col gap-8 items-center justify-center">
+        <main
+            class={classes!(
+                [ "max-w-[23.15rem] sm:max-w-xl md:max-w-3xl xl:max-w-7xl" ],
+                [ "flex flex-col gap-8 items-center justify-center" ]
+            )}
+        >
             <AvailabilityPill
                 available={false}
                 text_available="Open for Internships"
                 text_unavailable="Employed for Internship Position"
                 year={2025}
             />
-            <h1 class="text-[2.35rem] sm:text-[2.8rem] md:text-5xl xl:text-6xl text-center font-medium leading-[1.1]">
+            <h1
+                class={classes!(
+                    [ "text-[2.35rem] sm:text-[2.8rem] md:text-5xl xl:text-6xl" ],
+                    [ "text-center font-medium leading-[1.1]" ]
+                )}
+            >
                 { "I'm " }
                 <span class="underline-gradient">{ "Antonio" }</span>
                 { ", computer science student based in " }
                 <span class="text-gradient">{ "Austria" }</span>
             </h1>
-            <p class="sm:text-md xl:text-lg max-w-[20rem] sm:max-w-lg md:max-w-2xl xl:max-w-6xl text-center text-stone-400">
+            <p
+                class={classes!(
+                    [ "sm:text-md xl:text-lg" ],
+                    [ "max-w-[20rem] sm:max-w-lg md:max-w-2xl xl:max-w-6xl" ],
+                    [ "text-center text-stone-400" ]
+                )}
+            >
                 {
                     "A passionate Computer Science student
                     specializing in software development and project management,
